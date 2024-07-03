78c4cc1ee75115e342d5a54395fec5e2e74c21f4
川野辺 亮 <gggnbr@gmail.com>
2024-07-03 11:28:26 +0900

    feat: Padding以下を別Widgetに切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -25,34 +25,60 @@ class MyApp extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         fontSize: 18,
                         color: Color.fromARGB(250, 0, 80, 120)))),
-            body: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            body: const TweetTile()));
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1574198426593792000/f1Ba28Dm_400x400.jpg')),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
                 children: [
-                  CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://pbs.twimg.com/profile_images/1574198426593792000/f1Ba28Dm_400x400.jpg')),
-                  SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            'なんだかんだ @ベンゼマ',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(width: 8),
-                          Text('2024/07/01', style: TextStyle(fontSize: 16))
-                        ],
-                      ),
-                      SizedBox(height: 4),
-                      Text('コパアメリカとユーロと大忙し')
-                    ],
+                  Text(
+                    'なんだかんだ @ベンゼマ',
+                    style: TextStyle(fontSize: 16),
                   ),
+                  SizedBox(width: 8),
+                  Text('2024/07/01', style: TextStyle(fontSize: 16))
                 ],
               ),
-            )));
+              const SizedBox(height: 4),
+              const Text('コパアメリカとユーロと大忙し'),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite_border),
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite_border),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
